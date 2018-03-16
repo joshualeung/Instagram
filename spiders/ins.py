c9e02f6ed15c3bf1dc4fb523c9f639c4f106737f
joshualeung <joshualeung@tencent.com>
2018-03-16 18:18:45 +0800

    refactor.

--- spiders/ins.py
+++ spiders/ins.py
@@ -11,111 +11,30 @@ from scrapy.spiders import CrawlSpider,Rule
 import re
 import time
 from urllib import parse
+import traceback
 
 class InstagramSpider(CrawlSpider):
-    #define the name of spider
-    #define the domain of spider
-    #define the urls
     name = "Instagram"
     allowed_domains = ["instagram.com"]
     start_urls = [
         "https://www.instagram.com/hxmfamily/",
         "https://www.instagram.com/caitylotz/"
-                    #"https://www.instagram.com/nike",
-                  #"https://www.instagram.com/adidas",
-                  #"https://www.instagram.com/olympics/",
-                  #"https://www.instagram.com/teamcanada/",
-                  #"https://www.instagram.com/google",
-                  #"https://www.instagram.com/animal.co",
-                  #"https://www.instagram.com/car",
-                  #"https://www.instagram.com/flowerschannel",
-                  #"https://www.instagram.com/flowerschannel_",
-                  #"https://www.instagram.com/flower.channel/",
-                  #"https://www.instagram.com/theflowerchannel/",
-                  #"https://www.instagram.com/the_loft_flowers/",
-                  #"https://www.instagram.com/amaryllis_flowers/",
-                  #"https://www.instagram.com/sneakernews",
-                  #"https://www.instagram.com/boeing_747_pics/",
-                  #"https://www.instagram.com/boeing/",
-                  #"https://www.instagram.com/insta_dog/",
-                  #"https://www.instagram.com/dogs/",
-                  #"https://www.instagram.com/dogs_of_world_/",
-                  #"https://www.instagram.com/dogs.lovers/",
-                  #"https://www.instagram.com/lions/",
-                  #"https://www.instagram.com/bh_phones7/",
-                  #"https://www.instagram.com/windows/",
-                  #"https://www.instagram.com/gaming_laptop/",
-                  #"https://www.instagram.com/alienware/",
-                  #"https://www.instagram.com/airbus/",
-                  #"https://www.instagram.com/airbus_a380_lovers/",
-                  #"https://www.instagram.com/airbus_a380_800/",
-                  #"https://www.instagram.com/airbusa350xwb/",
-                  #"https://www.instagram.com/airbuslovers/",
-                  #"https://www.instagram.com/airbus_fanclub/",
-                  #"https://www.instagram.com/a350_production/",
-                  #"https://www.instagram.com/747.boeing/",
-                  #"https://www.instagram.com/boeinglovers/",
-                  #"https://www.instagram.com/airplaneslovers/",
-                  #"https://www.instagram.com/airplane2017/",
-                  #"https://www.instagram.com/airplanes.us/",
-                  #"https://www.instagram.com/deskspaces/",
-                  #"https://www.instagram.com/laptop.desktop.gadjet.murah/",
-                  #"https://www.instagram.com/isetups/",
-                  #"https://www.instagram.com/gaminglaptop/",
-                  #"https://www.instagram.com/laptop_second_murah/",
-                  #"https://www.instagram.com/ultrawidedesksetups/",
-                  #"https://www.instagram.com/desksetuptour/",
-                  #"https://www.instagram.com/desksetups_2016/",
-                  #"https://www.instagram.com/aestheticsetups/",
-                  #"https://www.instagram.com/minimalsetups/",
-                  #"https://www.instagram.com/samsung_id/",
-                  #"https://www.instagram.com/samsungmobile/",
-                  #"https://www.instagram.com/dailywatch/",
-                  #"https://www.instagram.com/watch/",
-                  #"https://www.instagram.com/rolex.watches/",
-                  #"https://www.instagram.com/mercedesbenz/",
-                  #"https://www.instagram.com/africa.lions/",
-                  #"https://www.instagram.com/africa_lions/",
-                  #"https://www.instagram.com/lions.africa/",
-                  #"https://www.instagram.com/wild_lions_/",
-                  #"https://www.instagram.com/lionswildusa/",
-                  #"https://www.instagram.com/statebicycleco/",
-                  #"https://www.instagram.com/thrill_bicycle/",
-                  #"https://www.instagram.com/trekbikes/",
-                  #"https://www.instagram.com/discoverlions/",
-                  #"https://www.instagram.com/lobbyforlions/",
-                  #"https://www.instagram.com/thelionworld/",
-                  #"https://www.instagram.com/savingthelion/",
-                  #"https://www.instagram.com/thelionstation/",
-                  #"https://www.instagram.com/sick_camera/",
-                  #"https://www.instagram.com/shahalamcamera/",
-                  #"https://www.instagram.com/dslr.shop/",
-                  #"https://www.instagram.com/store.camera/",
-                  #"https://www.instagram.com/camera_store_/",
-                  #"https://www.instagram.com/camerashop_th/",
-                  #"https://www.instagram.com/camera_shop78/",
-                  #"https://www.instagram.com/camera_shop/",
-                  #"https://www.instagram.com/camera_shop4you/",
-                       ]
+    ]
 
-    #rules = [
-    #    Rule(sle(allow=('?max_id\.php')), callback='parse'),
-    #]
     def parse(self, response):
-        # We get the json containing the photos's path
-        items = []
-        item = InstagramItem()
-        js = response.selector.xpath('//script[contains(., "window._sharedData")]/text()').extract()
-        js = js[0].replace("window._sharedData = ", "")
-        jscleaned = js[:-1]
-        ts = str(int(time.time()))
-        data = json.loads(jscleaned)
-        open(ts + ".json", "w").write(json.dumps(data))
-        user = data['entry_data']['ProfilePage'][0]['graphql']['user']
-        user_name = user['username']
+        """
+        解析用户主页内容
+        :param response:
+        :return:
+        """
+        json_body = response.selector.xpath('//script[contains(., "window._sharedData")]/text()').extract()[0].replace("window._sharedData = ", "")[:-1]
+        root = json.loads(json_body)
+        user = root['entry_data']['ProfilePage'][0]['graphql']['user']
+        username = user['username']
+        user_id = user['id']
         for edge in user['edge_owner_to_timeline_media']['edges']:
-            # print(edge.keys())
             node = edge['node']
+
             display_url = node['display_url']
             is_video = node['is_video']
             taken_at = node['taken_at_timestamp']
@@ -123,38 +42,57 @@ class InstagramSpider(CrawlSpider):
             ch = dim['height']
             cw = dim['width']
             id = node['id']
+            text = ""
+            try:
+                texts = [edge['node']['text'] for edge in node["edge_media_to_caption"]["edges"]]
+                text = "\n".join(texts)
+            except:
+                pass
             item = {
-                'username': user_name,
-                'image_url': display_url,
+                'user_name': username,
+                'user_id': user_id,
+                'media_id': id,
+                'media_url': display_url,
                 'is_video': is_video,
                 'taken_at': taken_at,
                 'ch': ch,
                 'cw': cw,
-                'id': id
+                'text': text
             }
             yield item
+
             if is_video:
                 url = "https://www.instagram.com/p/%s/?__a=1" % node['shortcode']
-                yield scrapy.Request(url, callback=self.parse_media)
-
-        has_next_page = user['edge_owner_to_timeline_media']['page_info']['has_next_page']
+                yield scrapy.Request(url, callback=self.parse_video, meta = {
+                    "user_name": username,
+                    "user_id": user_id
+                })
         #
-        user_id = user['id']
-        if has_next_page:
-            query_hash = "472f257a40c653c64c666ce877d59d2b"
+        if user['edge_owner_to_timeline_media']['page_info']['has_next_page']:
+            #query_hash = "472f257a40c653c64c666ce877d59d2b"
             end_cursor = user['edge_owner_to_timeline_media']['page_info']['end_cursor']
-            first = 12
-            params = {
-                'query_hash': query_hash,
-                'variables': '{"id":"%s","first":12,"after":"%s"}' % (user_id, end_cursor)
-            }
-            url = "https://www.instagram.com/graphql/query/?" + parse.urlencode(params)
-            yield scrapy.Request(url, callback=self.parse_next, meta = {'id': user_id, 'username': user_name})
+            # params = {
+            #     'query_hash': query_hash,
+            #     'variables': '{"id":"%s","first":12,"after":"%s"}' % (user_id, end_cursor)
+            # }
+            # url = "https://www.instagram.com/graphql/query/?" + parse.urlencode(params)
+            # yield scrapy.Request(url, callback=self.parse_next, meta = {'id': user_id, 'username': username})
+            time.sleep(1)
+            return self.start_next_page(user_id, end_cursor, meta = {'id': user_id, 'username': username})
+
+    def start_next_page(self, user_id, end_cursor, meta):
+        query_hash = "472f257a40c653c64c666ce877d59d2b"
+        params = {
+            'query_hash': query_hash,
+            'variables': '{"id":"%s","first":12,"after":"%s"}' % (user_id, end_cursor)
+        }
+        url = "https://www.instagram.com/graphql/query/?" + parse.urlencode(params)
+        yield scrapy.Request(url, callback=self.parse_next, meta = meta)
 
     def parse_next(self, response):
         data = json.loads(response.text)
         user_id = response.meta['id']
-        user_name = response.meta['username']
+        username = response.meta['username']
         timeline_media = data['data']['user']['edge_owner_to_timeline_media']
         for edge in timeline_media['edges']:
             node = edge['node']
@@ -165,41 +103,67 @@ class InstagramSpider(CrawlSpider):
             ch = dim['height']
             cw = dim['width']
             id = node['id']
+            # item = {
+            #     'username': username,
+            #     'image_url': display_url,
+            #     'is_video': is_video,
+            #     'taken_at': taken_at,
+            #     'ch': ch,
+            #     'cw': cw,
+            #     'id': id
+            # }
+            try:
+                texts = [edge['node']['text'] for edge in node["edge_media_to_caption"]["edges"]]
+                text = "\n".join(texts)
+            except:
+                pass
             item = {
-                'username': user_name,
-                'image_url': display_url,
+                'user_name': username,
+                'user_id': user_id,
+                'media_id': id,
+                'media_url': display_url,
                 'is_video': is_video,
                 'taken_at': taken_at,
                 'ch': ch,
                 'cw': cw,
-                'id': id
+                'text': text
             }
             yield item
             if is_video:
                 url = "https://www.instagram.com/p/%s/?__a=1" % node['shortcode']
-                yield scrapy.Request(url, callback=self.parse_media)
+                yield scrapy.Request(url, callback=self.parse_video)
 
 
         has_next_page = timeline_media['page_info']['has_next_page']
         if has_next_page:
-            query_hash = "472f257a40c653c64c666ce877d59d2b"
+            #query_hash = "472f257a40c653c64c666ce877d59d2b"
             end_cursor = timeline_media['page_info']['end_cursor']
-            first = 12
-            params = {
-                'query_hash': query_hash,
-                'variables': '{"id":"%s","first":12,"after":"%s"}' % (user_id, end_cursor)
-            }
-            url = "https://www.instagram.com/graphql/query/?" + parse.urlencode(params)
-            yield scrapy.Request(url, callback=self.parse_next, meta = {'id': user_id, 'username': user_name})
+            time.sleep(0.5)
+            return self.start_next_page(user_id, end_cursor, response.meta)
+            # first = 12
+            # params = {
+            #     'query_hash': query_hash,
+            #     'variables': '{"id":"%s","first":12,"after":"%s"}' % (user_id, end_cursor)
+            # }
+            # url = "https://www.instagram.com/graphql/query/?" + parse.urlencode(params)
+            # yield scrapy.Request(url, callback=self.parse_next, meta = {'id': user_id, 'username': user_name})
 
-    def parse_media(self, response):
-        data = json.loads(response.text)
-        media = data['graphql']['shortcode_media']
-        id = media['id']
-        shortcode = media['shortcode']
-        video_url = media['video_url']
-        yield {
-            'id': id,
-            'shortcode': shortcode,
-            'video_url': video_url
-        }
+    def parse_video(self, response):
+        """
+        解析视频信息
+        :param response:
+        :return:
+        """
+        try:
+            data = json.loads(response.text)
+            media = data['graphql']['shortcode_media']
+            yield {
+                "user_name": response.meta["user_name"],
+                "user_id": response.meta["user_id"],
+                'media_id': media['id'],
+                'shortcode': media['shortcode'],
+                'media_url': media['video_url'],
+                "is_video": True
+            }
+        except:
+            traceback.print_exc()
